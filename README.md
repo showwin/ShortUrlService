ae63c86a7385161883f1a0d3573fb3f595b0ab2b
showwin <showwin_kmc@yahoo.co.jp>
2014-10-29 21:49:51 +0800

    add coverall

---
+++ README.md
@@ -0,0 +1,5 @@
+ShortUrlService
+=======
+[![Build Status](https://travis-ci.org/showwin/ShortUrlService.svg?branch=master)](https://travis-ci.org/showwin/ShortUrlService)
+[![Coverage Status](https://coveralls.io/repos/showwin/ShortUrlService/badge.png)](https://coveralls.io/r/showwin/ShortUrlService)
+[![Code Climate](https://codeclimate.com/github/showwin/ShortUrlService/badges/gpa.svg)](https://codeclimate.com/github/showwin/ShortUrlService)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,20 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-#require 'coveralls'
-#Coveralls.wear!('rails')
+require 'coveralls'
+Coveralls.wear!('rails')
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-#require 'simplecov'
+require 'simplecov'
 
 
-=begin
+begin
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
 SimpleCov.start 'rails'
-=end
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
